5dfff60d1b7fdce6db7b1a19faace09e2a41814e
Long Cui <long.cui@miles.no>
2020-03-17 08:26:02 +0100

    Test basic LinQ (Take())

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,11 +8,14 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+
+        static readonly string[] Names = new string[3] {"Alice", "Bob", "Celina"};
+
+
         [TestMethod]
         public void TestMethod1()
         {
-            string[] names = new string[3] {"Alice", "Bob", "Celina"};
-            IEnumerable<string> linqQuery = from name in names
+            IEnumerable<string> linqQuery = from name in Names
                 where  name.StartsWith('A')
                 select name;
 
@@ -21,6 +24,19 @@ namespace UnitTestProject1
             {
                 Assert.AreEqual("Alice", name);
             }
+        }
+
+        //todo: any order in Take()?
+        [TestMethod]
+        public void TestTake()
+        {
+            IEnumerable<string> enumerable = from name in Names
+                select name;
+            
+            Assert.AreEqual(2, enumerable.Take(2).Count()); 
+            Assert.AreEqual(0, enumerable.Take(0).Count()); 
+            Assert.AreEqual(3, enumerable.Take(5).Count()); 
+
         }
     }
 }
