b0b0d4040d2ae0c3ba30021d7844abf80560ca69
Long Cui <long.cui@miles.no>
2020-05-28 09:05:41 +0200

    Thread example: need to investigate

---
+++ UnitTestProject1/Concept/Threading/ThreadingExample.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnitTest.Concept.Threading
+{
+    [TestClass]
+    public class ThreadingExample
+    {
+        [TestMethod]
+        public async Task TestAsync() {
+            Console.WriteLine(SynchronizationContext.Current);
+            await Task.Delay(10);
+            Console.WriteLine(SynchronizationContext.Current == null);
+        }
+
+        [TestMethod]
+        public async Task TestAsyncConfigureAwait()
+        {
+            for (int i = 0; i < 1990; i++) {
+                Console.WriteLine(SynchronizationContext.Current);
+                //await Task.Delay(10).ConfigureAwait(false);
+                await Task.Delay(10);
+                Console.WriteLine(SynchronizationContext.Current);
+                //Assert.IsFalse(SynchronizationContext.Current == null);
+            }
+        }
+    }
+}
