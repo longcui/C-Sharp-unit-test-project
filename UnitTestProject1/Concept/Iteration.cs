992ddfaaa8b79a8ffb1711634a9b12bb3fbdc153
Long Cui <long.cui@miles.no>
2020-06-08 10:01:41 +0200

    Used pattern matching

--- UnitTestProject1/Concept/Iteration.cs
+++ UnitTestProject1/Concept/Iteration.cs
@@ -11,14 +11,14 @@ namespace UnitTest.Concept
     {
         [TestMethod]
         public void TestAs() {
-            IEnumerable<int> numbers = new[] { 10, 20, 30 };
+            IEnumerable<int> numbers = new int[] { 10, 20, 30 };
             //below could NOT compile
             //Assert.AreEqual(40, numbers[0] + numbers[indexable.Count - 1]);
 
             //"as" could produce null.
-            IList<int> indexable = numbers as IList<int>;
             //Need to compare the result of the as expression with null to check if the conversion is successful
-            if (indexable != null) { 
+            if (numbers is IList<int> indexable)
+            {
                 Assert.AreEqual(40, indexable[0] + indexable[indexable.Count - 1]);
                 Assert.AreEqual(40, indexable.ElementAt(0) + indexable.ElementAt(indexable.Count - 1));
             }
